d825b6ef3ed2df3620558522ae8556684459f07c
KlosHiHi <vaceslavvlasov420@gmail.com>
2025-06-22 23:58:48 +0300

    1

---
+++ Model/RouteInfo.cs
@@ -0,0 +1,17 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace Trevoria.Model
+{
+    public class RouteInfo
+    {
+        public string Name { get; set; }
+
+        public List<Coordinate> Waypoints { get; set; } = new();
+
+        public string RouteType { get; set; }
+
+        public string MovementType { get; set; }
+
+        public string Description { get; set; }
+    }
+}
